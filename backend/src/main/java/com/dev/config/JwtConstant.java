caa0895d064ce2033f53cecdc9a7009d18f77076
caduoliveira01 <caduolivera17@gmail.com>
2025-03-06 23:06:54 -0300

    creatingn request and response

--- backend/src/main/java/com/dev/config/JwtConstant.java
+++ backend/src/main/java/com/dev/config/JwtConstant.java
@@ -1,6 +1,6 @@
 package com.dev.config;
 
 public class JwtConstant {
-    public static final String SECRETE_KEY="fahsuyfgafhdavhqutgqjdsafdasg";
+    public static final String SECRETE_KEY="fahsuyfgafhdavhqutgqjdsafdasgkojgihaasfajtghasugagasegsdae";
     public static final String JWT_HEADER="Authorization";
 }
--- backend/src/main/java/com/dev/controller/AuthController.java
+++ backend/src/main/java/com/dev/controller/AuthController.java
@@ -1,9 +1,23 @@
 package com.dev.controller;
 
+import com.dev.config.JwtProvider;
+import com.dev.model.User;
 import com.dev.repository.UserRepository;
+import com.dev.request.LoginRequest;
+import com.dev.response.AuthResponse;
 import com.dev.service.CustumeUserDetailsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +34,63 @@ public class AuthController {
     @Autowired
     private CustumeUserDetailsImpl custumeUserDetails;
 
+    @PostMapping("/signup")
+    public ResponseEntity<AuthResponse> createUserHandler(@RequestBody User user) throws Exception{
+        User isUserExists = userRepository.findByEmail(user.getEmail());
 
+        if (isUserExists!=null){
+            throw new Exception("email already exists with another account");
+        }
+
+        User createUser= new User();
+
+        createUser.setPassword(passwordEncoder.encode(user.getPassword()));
+        createUser.setEmail(user.getEmail());
+        createUser.setFullName(user.getFullName());
+
+        User savedUser=userRepository.save(createUser);
+
+        Authentication authentication= new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = JwtProvider.generateToken(authentication);
+
+        AuthResponse response = new AuthResponse();
+        response.setMessage("sign up success");
+        response.setJwt(jwt);
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/signing")
+    public ResponseEntity<AuthResponse> signing(@RequestBody LoginRequest loginRequest){
+        String username= loginRequest.getEmail();
+        String password=loginRequest.getPassword();
+
+        Authentication authentication= authenticate(username,password);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = JwtProvider.generateToken(authentication);
+
+        AuthResponse response = new AuthResponse();
+        response.setMessage("sign in success");
+        response.setJwt(jwt);
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+
+    }
+
+    private Authentication authenticate(String username, String password) {
+
+        UserDetails userDetails= custumeUserDetails.loadUserByUsername(username);
+
+        if (userDetails==null){
+            throw new BadCredentialsException("Invalid username");
+        }
+        if (!passwordEncoder.matches(password,userDetails.getPassword())){
+            throw new BadCredentialsException("invalid password");
+        }
+        return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+    }
 }
+
