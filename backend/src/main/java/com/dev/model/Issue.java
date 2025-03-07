a71e4f9178fb86f72a1621c4f90c9677efcf13f0
caduoliveira01 <caduolivera17@gmail.com>
2025-03-07 16:18:52 -0300

    creating entities for chat

--- backend/src/main/java/com/dev/model/Issue.java
+++ backend/src/main/java/com/dev/model/Issue.java
@@ -1,7 +1,12 @@
 package com.dev.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
@@ -11,6 +16,22 @@ public class Issue {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
+    private String title;
+    private String description;
+    private String status;
+    private Long projectId;
+    private String priority;
+    private LocalDate dueDate;
+    private List<String> tags= new ArrayList<>();
+
     @ManyToOne
     private User assignee;
+
+    @JsonIgnore
+    @ManyToOne
+    private Project project;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "issue", cascade = CascadeType.ALL,orphanRemoval = true)
+    private List<Comment> comments= new ArrayList<>();
 }
